bd15d76f9190b646214cf9046fe21c0c86dbb7f9
amit-15 <amitkumartiwaree.akt@gmailcom>
2022-04-21 12:50:02 +0530

    workflow added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ PROJECT_NAME ="oneNuron_pypi"
 USER_NAME ="amit15"
 
 setuptools.setup(
-    name="f"{PROJECT_NAME}-{USER_NAME}",
+    name=f"{PROJECT_NAME}-{USER_NAME}",
     version="0.0.1",
     author="USER_NAME",
     author_email="author@example.com",
